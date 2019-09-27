c423ed12f1f35c8a6896e85f0102b981bdc19e06
王强 <249749871@qq.com>
2019-09-27 09:54:13 +0800

    update

--- magicwallet-bts-pay.js
+++ magicwallet-bts-pay.js
@@ -1,56 +1,31 @@
 (function (window) {
-  if (!window) return;
+  window.magicWalletBtsPay = window.magicWalletBtsPay || {};
 
-  var magicwallet_bts_account;
 
-  var magicwallet_send_bts_account = function (data) {
-    magicwallet_bts_account = data;
-  }
+  //APP发送BTS账号信息
+  magicWalletBtsPay.magicwalletSendBtsAccount = function (data) {
+    magicWalletBtsPay.magicwallet_bts_account = data;
+  };
 
-  var magicwalletGetBtsAccount = function () {
-    return new Promise(function (resolve, reject) {
-      if (!magicwallet_bts_account) {
-        var get_bts_account_interval = setInterval(function () {
-          var bts_account = magicwallet_bts_account;
-          if (bts_account) {
-            resolve(bts_account);
-            clearInterval(get_bts_account_interval);
-          }
-        }, 1000)
 
-        setTimeout(() => {
-          clearInterval(get_bts_account_interval);
-        }, 15000);
-      } else {
-        resolve(magicwallet_bts_account)
-      }
-    })
-  }
-
-  var magicwallet_timeout_promise = function (ms) {
-    return new Promise(function (resolve, reject) {
-      setTimeout(resolve, ms);
-    }).then(function () {
-      throw ("error");
-    });
-  }
-
-  var magicwallet_get_bts_account = function () {
+  //获取BTS账号信息
+  magicWalletBtsPay.magicwalletGetBtsAccount = function () {
     return Promise.race([
-      magicwalletGetBtsAccount(),
-      magicwallet_timeout_promise(15000),
+      magicWalletBtsPay._magicwalletGetBtsAccount(),
+      magicWalletBtsPay._magicwalletTimeoutPromise(15000),
     ])
   }
 
-
-  var magicwallet_confirm_pay = function (pay_account, pay_asset, pay_amount, order_id) {
+  //确认支付
+  magicWalletBtsPay.magicwalletConfirmPay = function (pay_account, pay_asset, pay_amount, order_id) {
     if (!pay_account || !pay_asset || !pay_amount || !order_id) {
       return
     }
     location.href = "https://magicwallet-btspay.magicw.net?pay_account=" + pay_account + "&pay_asset=" + pay_asset + "&pay_amount=" + pay_amount + "&order_id=" + order_id;
   }
 
-  var magicwallet_get_pay_params = function getParams() {
+  //APP端获取支付信息
+  magicWalletBtsPay.magicwalletGetPayParams = function () {
     var url = window.location.search;
     var theRequest = new Object();
     if (url.indexOf("?") != -1) {
@@ -63,7 +38,8 @@
     return theRequest;
   }
 
-  var magicwallet_close_button = function (platform) {
+  //关闭按钮
+  magicWalletBtsPay.magicwalletCloseButton = function (platform) {
     if (!platform) {
       return
     }
@@ -75,9 +51,30 @@
     }
   }
 
-  window.magicwallet_send_bts_account = magicwallet_send_bts_account;
-  window.magicwallet_get_bts_account = magicwallet_get_bts_account;
-  window.magicwallet_confirm_pay = magicwallet_confirm_pay;
-  window.magicwallet_close_button = magicwallet_close_button;
-  window.magicwallet_get_pay_params = magicwallet_get_pay_params;
-})(window)
+  magicWalletBtsPay._magicwalletGetBtsAccount = function () {
+    return new Promise(function (resolve, reject) {
+      if (!magicWalletBtsPay.magicwallet_bts_account) {
+        var get_bts_account_interval = setInterval(function () {
+          if (magicWalletBtsPay.magicwallet_bts_account) {
+            resolve(magicWalletBtsPay.magicwallet_bts_account);
+            clearInterval(get_bts_account_interval);
+          }
+        }, 1000)
+
+        setTimeout(() => {
+          clearInterval(get_bts_account_interval);
+        }, 15000);
+      } else {
+        resolve(magicWalletBtsPay.magicwallet_send_bts_account.prototype.magicwallet_bts_account)
+      }
+    })
+  }
+
+  magicWalletBtsPay._magicwalletTimeoutPromise = function (ms) {
+    return new Promise(function (resolve, reject) {
+      setTimeout(resolve, ms);
+    }).then(function () {
+      throw ("error");
+    });
+  }
+}(window));
